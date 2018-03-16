913aa884e10553abd4e9fc13d2bfc7e4682b5a15
Mario Pongibove <mario.pongibove@dcsg.com>
2018-03-16 17:09:46 -0400

    fixed pojo

--- dsg-fulfillment-optimus/src/main/java/com/dcsg/fulfillment/optimus/models/PaymentDetails.java
+++ dsg-fulfillment-optimus/src/main/java/com/dcsg/fulfillment/optimus/models/PaymentDetails.java
@@ -1,14 +1,28 @@
 
 package com.dcsg.fulfillment.optimus.models;
 
+import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-
+    "PaymentDetail"
 })
 public class PaymentDetails {
 
+    @JsonProperty("PaymentDetail")
+    private List<PaymentDetail> paymentDetail = null;
+
+    @JsonProperty("PaymentDetail")
+    public List<PaymentDetail> getPaymentDetail() {
+        return paymentDetail;
+    }
+
+    @JsonProperty("PaymentDetail")
+    public void setPaymentDetail(List<PaymentDetail> paymentDetail) {
+        this.paymentDetail = paymentDetail;
+    }
 
 }
